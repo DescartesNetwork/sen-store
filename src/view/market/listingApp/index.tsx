a0de59542f1df8e72b6b4ca848492106e2741c67
thiennv <thiennguyen@descartes.network>
2022-07-26 22:21:27 +0700

    integrate app

--- src/view/market/listingApp/index.tsx
+++ src/view/market/listingApp/index.tsx
@@ -1,6 +1,6 @@
 import { useRegister, useUI } from '@sentre/senhub'
 
-import { Col, Row, Space } from 'antd'
+import { Card, Col, Row, Space } from 'antd'
 import IntegrationCard from './integrationCard'
 import InfiniteSlideIcon from './infiniteSlideIcon'
 
@@ -10,27 +10,38 @@ const ListingApp = () => {
   const register = useRegister()
   const appIds = Object.keys(register)
   const {
-    ui: { theme },
+    ui: { theme, width },
   } = useUI()
 
   const isLightTheme = theme === 'light'
+  const isMobile = width < 768
+  const slideStyle = isMobile
+    ? { padding: '0 12px' }
+    : { padding: 0, marginLeft: -12 }
+  const paddingBottom = isMobile ? { paddingBottom: 24 } : {}
 
   return (
-    <Row
-      gutter={[24, 24]}
-      style={{ background: isLightTheme ? '#EAE6F5' : '#09090D' }}
-      align="middle"
+    <Card
+      bordered={false}
+      style={{
+        height: '100%',
+        background: isLightTheme ? '#EAE6F5' : '#09090D',
+        boxShadow: 'unset',
+      }}
+      bodyStyle={{ padding: 0, ...paddingBottom }}
     >
-      <Col span={12}>
-        <IntegrationCard />
-      </Col>
-      <Col span={12}>
-        <Space direction="vertical" style={{ width: '100%' }}>
-          <InfiniteSlideIcon appIds={appIds} />
-          <InfiniteSlideIcon appIds={appIds} reverse />
-        </Space>
-      </Col>
-    </Row>
+      <Row gutter={[24, 24]} align="middle">
+        <Col xs={24} md={12}>
+          <IntegrationCard />
+        </Col>
+        <Col xs={24} md={12} style={{ ...slideStyle }}>
+          <Space direction="vertical" style={{ width: '100%' }}>
+            <InfiniteSlideIcon appIds={appIds} />
+            <InfiniteSlideIcon appIds={appIds} reverse />
+          </Space>
+        </Col>
+      </Row>
+    </Card>
   )
 }
 
